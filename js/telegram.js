9ba018e115d5973646a0408ca92d1f2d765d0a69
adina shaidieva <ashaydieva13@gmail.com>
2022-09-20 23:59:22 +0600

    change

--- js/telegram.js
+++ js/telegram.js
@@ -11,10 +11,11 @@ const telegramApply = () =>{
         let bot = {
             TOKEN:'5791665278:AAFi8hbwi-EeBbkhdIsirRPPDbKPFPaNzSk',
             chatID:'-745203987'
+            
         }
 
         const telegramFetch = () =>{
-            const sendTelegram = `Имя: ${message.value} и Телефон: ${number.value}` 
+            const sendTelegram = `Name: ${message.value} и Phone: ${number.value}` 
             fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${sendTelegram}`,
                 {
                     method:'GET'
@@ -25,15 +26,16 @@ const telegramApply = () =>{
                 alert('ERROR')
             }
             )
+          
         }
-    
+    telegramFetch()
         
     })
-    const modalFunc=()=>{
+    const modalTel=()=>{
         const button = document.querySelector('.telegram__button')
         const modalCloseButton = document.querySelector('.modalCloseButton')
-        const modal = document.querySelector('.modal')
-        const backdrop = document.querySelector('.backdrop')
+        const modal = document.querySelector('.modalTel')
+        const backdrop = document.querySelector('.backdropTel')
         const content = document.querySelector('.modalContent')
     
     
@@ -58,6 +60,7 @@ const telegramApply = () =>{
         })  
             setTimeout(removeClasses,2000)
     }
+    modalTel()
 }
 
 telegramApply()
